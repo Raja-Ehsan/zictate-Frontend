a7641952c57d29ae83113989a003c378872cae0d
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2022-08-05 01:46:21 -0700

    make it better

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,7 @@ import refresh from "../auth/auth"
 export default function Home() {
   const currentUser = JSON.parse(sessionStorage.getItem("User"));
   const navigate = useNavigate();
-  console.log(currentUser)
-  if(!currentUser) navigate('/401')
+  if(currentUser===null) navigate('/401')
   // React.useEffect(() => {
   //   fetch('https://zictate.herokuapp.com/refresh').then(res => {
   //     if (res.status === 401) 
