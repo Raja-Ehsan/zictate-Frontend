906ad41539791e562a6de9198ad0e2193e275694
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2022-08-05 02:01:13 -0700

    make it better

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
   console.log(currentUser)
   if(currentUser===null) {
     console.log("lulul")
-    navigate('/401') 
+    this.props.history.push('/401')
     console.log("lulul")
   }
   // React.useEffect(() => {
